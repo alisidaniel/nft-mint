0f99687c05aa66c4dacb14dabf6b77278abd183a
alisidaniel <alisidaniel@gmail.com>
2022-01-03 09:46:02 +0200

    gitInore deploy and contract was added

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/hhFaKed2N92FhUeqktXcIMqPZHoxccRb',
+      accounts: ['efbc21b37fae89c30d4ea4e616d66554749921084ad8545c255f2925ee96a26c'],
+    },
+  },
 };
---
+++ scripts/run.js
@@ -0,0 +1,28 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+
+    // fisrt mint
+    let txn = await nftContract.makeAnEpicNFT()
+    await txn.wait()
+
+    // second mint
+    txn = await nftContract.makeAnEpicNFT()
+    await txn.wait()
+  };
+
+
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
